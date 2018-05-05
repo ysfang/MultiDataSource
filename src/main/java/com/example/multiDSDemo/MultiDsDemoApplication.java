ed0f5bef5dd58831df189d1d796a17f5000feca2
ysfang <ysfang82@gmail.com>
2018-05-05 15:23:50 +0800

    Add contents from spring initalizr

---
+++ src/main/java/com/example/multiDSDemo/MultiDsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.multiDSDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDsDemoApplication.class, args);
+	}
+}
