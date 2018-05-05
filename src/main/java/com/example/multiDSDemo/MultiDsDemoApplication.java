71a2a0d7a821a7a7328d740fb308aa992a8fc283
ysfang <ysfang82@gmail.com>
2018-05-05 20:15:50 +0800

    Refactoring class name

--- src/main/java/com/example/multiDSDemo/MultiDsDemoApplication.java
+++ src/main/java/com/example/multiDSDemo/MultiDsDemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiDsDemoApplication {
+public class MultiDSDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MultiDsDemoApplication.class, args);
+		SpringApplication.run(MultiDSDemoApplication.class, args);
 	}
 }
