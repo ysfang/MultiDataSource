355a42f6e539d98342bc1e2bee1008ca21b3f152
ysfang <ysfang82@gmail.com>
2018-05-14 10:27:20 +0800

    Remove transaction manager

--- src/main/java/com/example/mybatisspringdemo/config/DataSource1Config.java
+++ src/main/java/com/example/mybatisspringdemo/config/DataSource1Config.java
@@ -8,27 +8,17 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 @MapperScan(basePackages = "com.example.mybatisspringdemo.dao.ds1", sqlSessionFactoryRef = "sqlSessionFactoryBean1")
 public class DataSource1Config {
-    public static final String TXN_MANAGER = "txnManager";
-
     @Bean(name = "datasource1")
     @Primary
     @ConfigurationProperties(prefix = "spring.datasource1")
     public DataSource dataSource1() {
         return DataSourceBuilder.create().build();
-    }
-
-    @Bean(name = TXN_MANAGER)
-    @Primary
-    public PlatformTransactionManager txnManager() {
-        return new DataSourceTransactionManager(dataSource1());
     }
 
     @Bean(name = "sqlSessionFactoryBean1")
